ae1ecc05dfea0dc236c965d02c6b950a7dbbbbd0
jrodriguezs92 <jrodriguezs0292@gmail.com>
2018-03-14 20:03:39 -0600

    MUX
    Agregado MUX

--- ALU.sv
+++ ALU.sv
@@ -2,13 +2,13 @@ module ALU
 			#(parameter width = 3)
 			 (input logic [width-1:0] a,
 			  input logic [width-1:0] b,
-			  input logic
+			  input logic [3:0] control,
 			  output logic [width-1:0] result,
 			  output logic n,
 			  output logic z,
 			  output logic c,
 			  output logic v);
-
+	
 	
 
 endmodule
