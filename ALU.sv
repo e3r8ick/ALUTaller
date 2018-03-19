866b26d4ddb601bd1214b25ba7ba7aab74bf21e5
Santiago Gamboa Ramírez <santigr17@gmail.com>
2018-03-19 12:22:23 -0600

    Add files via upload

--- ALU.sv
+++ ALU.sv
@@ -10,94 +10,12 @@ module ALU
 			  output logic c,
 			  output logic v);
 			  
-	//logic [width-1:0] z;
-	/*
-	generate
-		if(control == 0) begin
-			sumadorNbits 
-			#(width) 
-			sumaN(a,b,carryin,c,result);
-		end
-		else if(control == 1) begin
-			sllN
-			#(width)
-			shiftL(a,b,result);
-		end
-		else if(control == 2) begin
-			srlN
-			#(width)
-			shiftR(a,b,result);
-		end
-		else if(control == 3) begin
-			xorN
-			#(width)
-			xorgate(a,b,result);
-		end
-		endgenerate*/
-		
-	/*
-	always_comb
-		case(control)
-		0: 
-				sumadorNbits
-				#(width)
-				sumaN(a,b,carryin,c,result);
-			
-				
-		1:
-				sllN
-				#(width)
-				shiftL(a,b,result);
-		
-			
-		2:
-				srlN
-				#(width)
-				shiftR(a,b,result);
-		
-		
-		3: 
-				xorN
-				#(width)
-				xorgate(a,b,result);
-				
-		
-		4: 
-				orN
-				#(width)
-				orgate(a,b,result);
-		
-		5:
-				notN
-				#(width)
-				notgate(a,result);
-				
-		6: 
-				andN
-				#(width)
-				andgate(a,b,result);
-			
-		
-		7: 
-				andN
-				#(width)
-				andgate(a,b,result);
-			
-		
-		default: 
-						andN
-						#(width)
-						andgate(a,b,result);
-						
-		endcase*/
-		
-	logic [width-1:0] d0,d1,d2,d3,d4,d5,d6;
-	
-	generate
+	logic [width-1:0] d0,d1,d2,d3,d4,d5,d6, d7;
+	wire cOut = c;
 		
 		sumadorNbits 
 		#(width)
-		sumaN(a,b,carryin,c,d0);
+		sumaN(a,b,carryin,cOut,d0);
 		
 		sllN
 		#(width)
@@ -123,8 +41,12 @@ module ALU
 		#(width)
 		andgate(a,b,d6);
 		
-		//falta restador
-		
-	endgenerate
+		restadorNbits
+		#(width)
+		restar(a, b, d7, z, n, cOut, v);
+
+		MUX
+		#(width)
+		muxOut(d0, d1, d2, d3, d4, d5, d6, d7, control, result);
 
 endmodule
