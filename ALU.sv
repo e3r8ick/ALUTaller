9f1022f3475bc3cfb364c6bbfa21cb2cb2d4d6cd
jrodriguezs92 <jrodriguezs0292@gmail.com>
2018-03-19 17:03:28 -0600

    ALU cambio
    ALU

--- ALU.sv
+++ ALU.sv
@@ -10,6 +10,50 @@ module ALU
 			  output logic c,
 			  output logic v);
 			  
+	
+	logic [width-1:0] d0,d1,d2,d3,d4,d5,d6,d7; //d7 : resultado para restador
+	
+	generate
+		
+		sumadorNbits 
+		#(width)
+		sumaN(a,b,carryin,c,d0);
+		
+		sllN
+		#(width)
+		shiftL(a,b,d1);
+		
+		srlN
+		#(width)
+		shiftR(a,b,d2);
+		
+		xorN
+		#(width)
+		xorgate(a,b,d3);
+		
+		orN
+		#(width)
+		orgate(a,b,d4);
+		
+		notN
+		#(width)
+		notgate(a,d5);
+		
+		andN
+		#(width)
+		andgate(a,b,d6);
+		
+		//falta restador
+		
+		MUX
+		#(width)
+		muxresult(d0,d1,d2,d3,d4,d5,d6,d7,control,result);
+		
+	endgenerate
+	
+
+endmodule
+
 	//logic [width-1:0] z;
 	/*
 	generate
@@ -90,41 +134,3 @@ module ALU
 						andgate(a,b,result);
 						
 		endcase*/
-		
-	logic [width-1:0] d0,d1,d2,d3,d4,d5,d6;
-	
-	generate
-		
-		sumadorNbits 
-		#(width)
-		sumaN(a,b,carryin,c,d0);
-		
-		sllN
-		#(width)
-		shiftL(a,b,d1);
-		
-		srlN
-		#(width)
-		shiftR(a,b,d2);
-		
-		xorN
-		#(width)
-		xorgate(a,b,d3);
-		
-		orN
-		#(width)
-		orgate(a,b,d4);
-		
-		notN
-		#(width)
-		notgate(a,d5);
-		
-		andN
-		#(width)
-		andgate(a,b,d6);
-		
-		//falta restador
-		
-	endgenerate
-
-endmodule
